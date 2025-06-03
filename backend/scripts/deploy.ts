ea41666f4ae464e763f1847ce83d387801f4552d
cosmeak <guillaume.fine@cosmeak.dev>
2025-06-03 14:07:02 +0200

    feat: deploy contract script

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,14 +1,14 @@
-import hre from "hardhat";
+import { ethers } from "hardhat";
 
 async function main() {
-  const Safe = await hre.ethers.getContractFactory("Safe");
-  const safeInstance = await Safe.deploy();
+  const Contract = await ethers.getContractFactory("Ballot");
+  const contractInstance = await Contract.deploy();
 
-  await safeInstance.waitForDeployment();
-  const [owner] = await hre.ethers.getSigners();
+  await contractInstance.waitForDeployment();
+  const [owner] = await ethers.getSigners();
   console.log("Deploying contract with address:", owner.address);
 
-  const contractAddress = await safeInstane.getAddress();
+  const contractAddress = await contractInstane.getAddress();
   console.log(`Contract deployed at: ${contractAddress}`);
 }
 
